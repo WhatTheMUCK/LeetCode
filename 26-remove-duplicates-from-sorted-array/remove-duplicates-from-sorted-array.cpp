9064a6ab1c969ea9f06c94e42d655542619583aa
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-05-10 16:45:38 +0300

    Time: 5 ms (78.03%) | Memory: 21 MB (64.36%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 1;
+        int uniq = 0;
+        while (i < nums.size()){
+            if (nums[i] == nums[uniq]){
+                i++;
+            } else {
+                uniq++;
+                nums[uniq] = nums[i]; 
+            }
+        }
+        return uniq + 1;
+    }
+};
