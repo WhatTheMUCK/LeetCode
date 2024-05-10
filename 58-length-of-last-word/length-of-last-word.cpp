e07534b60accfd1bda36504260fd60ccfd74015b
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-05-10 16:59:33 +0300

    Time: 0 ms (100.00%) | Memory: 8.1 MB (5.32%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream input(s);
+        string in;
+        while (input >> in);
+        return in.size();
+    }
+    // Проверка
+};
