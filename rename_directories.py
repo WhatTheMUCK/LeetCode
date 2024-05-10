8da89b6ef760e7d004c17f445e4d7509a42e63ad
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-05-10 16:11:29 +0300

    Update rename_directories.py

--- rename_directories.py
+++ rename_directories.py
@@ -2,7 +2,11 @@ import os
 import re
 
 def rename_directories(path):
+    pattern = re.compile(r'^\d{4}-.+$')  # Проверяем, начинается ли имя с четырёх цифр и дефиса
     for dir_name in os.listdir(path):
+        if pattern.match(dir_name):
+            continue  # Если директория уже соответствует формату, пропускаем её
+
         full_dir_path = os.path.join(path, dir_name)
         if os.path.isdir(full_dir_path):
             match = re.match(r'^(\d+)(-.*)$', dir_name)
