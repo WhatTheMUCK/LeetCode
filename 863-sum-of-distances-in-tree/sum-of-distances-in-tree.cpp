f8d9e5c91be35d9d6a03d35805ea9ea4e39bb250
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-05-10 20:49:12 +0300

    Time: 563 ms (5.18%) | Memory: 121.4 MB (13.35%) - LeetSync

---
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> graph;
+    vector<int> count;
+    vector<int> res;
+
+    void dfs(int node, int parent){
+        for (int child : graph[node]){
+            if (child != parent){
+                dfs(child, node);
+                count[node] += count[child];
+                res[node] += res[child] + count[child];
+            }
+        }
+    }
+
+    void dfs2(int node, int parent){
+        for (int child : graph[node]){
+            if (child != parent){
+                res[child] = res[node] - count[child] + (count.size() - count[child]);
+                dfs2(child, node);
+            }
+        }
+    }
+
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        graph.clear();
+        for (vector<int> elem : edges){
+            graph[elem[0]].push_back(elem[1]);
+            graph[elem[1]].push_back(elem[0]);
+        }
+        count = vector<int>(n, 1);
+        res = vector<int>(n, 0);
+
+        dfs(0, -1);
+        dfs2(0, -1);
+        
+        return res;
+    }
+};
