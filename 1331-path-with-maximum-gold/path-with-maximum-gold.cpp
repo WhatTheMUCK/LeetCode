7f71216c3d3f88b14072e15617a9f0cae20abc66
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-05-14 13:11:47 +0300

    Time: 150 ms (53.03%) | Memory: 9.1 MB (81.32%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int DFS(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &visited, int sum) {
+    vector<int> roww = {1, -1, 0, 0};
+    vector<int> coll = {0, 0, -1, 1};
+    int maxGold = 0;
+
+    int DFS(int x, int y, vector<vector<int>> &grid) {
         // Проверка границ должна быть первой
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0 || visited[i][j] == 1)
-            return sum;
-        sum += grid[i][j];
-        visited[i][j] = 1;
-        int maxGold = sum;
-        // Переменные для хранения промежуточных результатов
-        int down = DFS(i + 1, j, grid, visited, sum);
-        int right = DFS(i, j + 1, grid, visited, sum);
-        int up = DFS(i - 1, j, grid, visited, sum);
-        int left = DFS(i, j - 1, grid, visited, sum);
-        maxGold = max({maxGold, down, right, up, left});
-        visited[i][j] = 0; // Важно сбросить посещение для других путей
-        return maxGold;
+        if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == 0)
+            return 0;
+        int curr = grid[x][y];
+        grid[x][y] = 0;
+        int localMaxGold = curr;
+
+        for (int i = 0; i < 4; i++) {
+            int newX = x + roww[i];
+            int newY = y + coll[i];
+            localMaxGold = max(localMaxGold, curr + DFS(newX, newY, grid));
+        }
+
+        grid[x][y] = curr;
+        return localMaxGold;
     }
 
     int getMaximumGold(vector<vector<int>>& grid) {
@@ -23,7 +28,7 @@ public:
         for (int i = 0 ; i < grid.size(); i++){
             for (int j = 0; j < grid[0].size(); j++){
                 if (grid[i][j] != 0)
-                    answer = max(answer, DFS(i, j, grid, visited, 0));
+                    answer = max(answer, DFS(i, j, grid));
             }
         }
         return answer;
