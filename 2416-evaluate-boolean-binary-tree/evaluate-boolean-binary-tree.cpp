7f6ce8e47bb2fd6372a57206feafb4b17789ec17
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-05-16 16:22:27 +0300

    Time: 9 ms (66.76%) | Memory: 17.2 MB (92.60%) - LeetSync

---
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool DFS(TreeNode* root){
+        if (root->val < 2)
+            return root->val;
+        if (root->val == 2)
+            return DFS(root->left) || DFS(root->right);
+        return DFS(root->left) && DFS(root->right);
+    }
+    bool evaluateTree(TreeNode* root) {
+        return DFS(root);
+    }
+};
