0600b72bda097f5f293bd4f0c90541572d9ebac3
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-05-20 18:13:00 +0300

    Time: 3 ms (68.97%) | Memory: 8.3 MB (65.34%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        int n = nums.size();
-        int bitmask = pow(2, n) - 1;
-        int answer = 0, temp;
-        while (bitmask > 0){
-            temp = 0;
-            for (int i = 0; i < n; i++){
-                if (bitmask & (1 << i)){
-                    temp ^= nums[n - (i + 1)];
-                }
-            }
-            answer += temp;
-            bitmask--;
-        }
-        return answer;
+        return accumulate(nums.begin(), nums.end(), 0, [](int acc, int x){ return acc | x; }) << ((int)nums.size() - 1);
     }
 };
