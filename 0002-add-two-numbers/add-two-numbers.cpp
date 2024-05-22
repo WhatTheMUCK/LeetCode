65c928af10fa4445b813f17f6ef144be719d2ccd
GitHub Action <action@github.com>
2024-05-22 14:20:09 +0000

    Automatically rename directories

--- 0002-add-two-numbers/add-two-numbers.cpp
+++ 0002-add-two-numbers/add-two-numbers.cpp
@@ -11,19 +11,21 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *dummyHead = new ListNode(-1);
-        ListNode *cur = dummyHead;
-        int sum = 0, first, second;
-        while (l1 || l2 || sum){
-            first = (l1 ? l1->val : 0);
-            second = (l2 ? l2->val : 0);
-            sum += first + second;
+        int sum = 0, val1, val2;
+        ListNode* dummyHead = new ListNode(-1);
+        ListNode* cur = dummyHead;
+        while (l1 || l2){
+            val1 = l1 == nullptr ? 0 : l1->val;
+            val2 = l2 == nullptr ? 0 : l2->val;
+            sum += val1 + val2;
             cur->next = new ListNode(sum % 10);
             cur = cur->next;
             sum /= 10;
-            l1 = (l1 ? l1->next : l1);
-            l2 = (l2 ? l2->next : l2);
+            l1 = l1 == nullptr ? l1 : l1->next; 
+            l2 = l2 == nullptr ? l2 : l2->next; 
         }
+        if (sum)
+            cur->next = new ListNode(sum);
         return dummyHead->next;
     }
 };
