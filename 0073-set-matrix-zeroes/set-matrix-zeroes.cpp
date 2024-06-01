7292a89f2cd9400ad6de94cae6a494d621510b75
GitHub Action <action@github.com>
2024-06-01 19:57:52 +0000

    Automatically rename directories

--- 0073-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> zeros;
-        for (int i = 0; i < matrix.size(); i++){
-            for (int j = 0; j < matrix[i].size(); j++){
-                if (matrix[i][j] == 0)
-                    zeros.push_back({i, j});
+        int n = matrix.size();
+        int m = matrix[0].size();
+        bool firstRow = false, firstCol = false;
+        for (int i = 0; i < n; i++){
+            if (matrix[i][0] == 0){
+                firstCol = true;
+                break;
             }
         }
-        
-        // for (int i = 0; i < zeros.size(); i++){
-        //     cout << zeros[i][0] << " " << zeros[i][1] << "\n";
-        // }
 
-        vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        for (vector<int> zero : zeros){
-            int x, y;
-            for (int i = 0; i < directions.size(); i++){
-                x = zero[1];
-                y = zero[0];
-                while (x >= 0 && x < matrix[0].size() && y >= 0 && y < matrix.size()){
-                    matrix[y][x] = 0;
-                    x += directions[i][1];
-                    y += directions[i][0];
+        for (int j = 0; j < m; j++){
+            if (matrix[0][j] == 0){
+                firstRow = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < n; i++){
+            for (int j = 1; j < m; j++){
+                if (matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
-             
+        }
+
+        for (int i = 1; i < n; i++){
+            for (int j = 1; j < m; j++){
+                if (matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstCol){
+            for (int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (firstRow){
+            for (int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
         }
     }
 };
