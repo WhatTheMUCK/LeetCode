1a01749c9a0039d863f60e8efd1068f3ea2c936e
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-01 18:10:39 +0300

    Time: 6 ms (28.49%) | Memory: 11.3 MB (38.67%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -4,9 +4,9 @@ public:
         unordered_map<int, int> occur;
         for (int elem : arr)
             occur[elem]++;
-        vector<int> helper;
-        generate_n(back_inserter(helper), occur.size(), [it = occur.begin()]() mutable { return (it++)->second; });
-        sort(helper.begin(), helper.end());
-        return unique(helper.begin(), helper.end()) == helper.end();
+        unordered_set<int> helper;
+        for (pair<int, int> elem : occur)
+            helper.insert(elem.second);
+        return occur.size() == helper.size();
     }
 };
