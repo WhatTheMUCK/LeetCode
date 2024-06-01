9ca2d7c443426e4ac1d061d605777169df204abe
GitHub Action <action@github.com>
2024-06-01 13:54:34 +0000

    Automatically rename directories

--- 0349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -3,9 +3,23 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> set1(all(nums1)), set2(all(nums2));
         vector<int> answer;
-        set_intersection(all(set1), all(set2), back_inserter(answer));
+        sort(all(nums1));
+        sort(all(nums2));
+        int first = 0, second = 0;
+        while (first < nums1.size() && second < nums2.size()){
+            if (nums1[first] == nums2[second]){
+                answer.push_back(nums1[first]);
+                first = upper_bound(all(nums1), nums1[first]) - nums1.begin();
+                second = upper_bound(all(nums2), nums2[second]) - nums2.begin();
+            } else {
+                if (nums1[first] < nums2[second]){
+                    first++;
+                } else {
+                    second++;
+                }
+            }
+        }
         return answer;
     }
 };
