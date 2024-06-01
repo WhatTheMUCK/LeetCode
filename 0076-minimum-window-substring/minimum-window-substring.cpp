01248b4a9ac82903815636dfd0b0b3c3ac44b079
GitHub Action <action@github.com>
2024-06-01 17:02:00 +0000

    Automatically rename directories

--- 0076-minimum-window-substring/minimum-window-substring.cpp
+++ 0076-minimum-window-substring/minimum-window-substring.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        string answer = "";
-        if (t.size() > s.size())
-            return answer;
         unordered_map<char, int> dict;
         for (char elem : t){
             dict[elem]++;
         }
-        int minimum = s.size() + 1, index;
-        int diff = t.size();
-        int l = 0, r = 0;
+        int checker = t.size();
+        int l = 0, r = 0, minSize = INT_MAX, startIndex = -1;
         while (l < s.size()){
-            while (diff > 0 && r < s.size()){
-                if (dict.find(s[r]) != dict.end()){
-                    dict[s[r]]--;
-                    if (dict[s[r]] >= 0)
-                        diff--;
-                }
+            if (r < s.size() && checker > 0){
+                if (dict[s[r]] > 0)
+                    checker--;
+                dict[s[r]]--;
                 r++;
-            }
-            if (diff == 0 && r - l < minimum){
-                minimum = r - l;
-                index = l;
-            }
-            if (dict.find(s[l]) != dict.end()){
+            } else {
+                if (checker == 0){
+                    if (r - l < minSize){
+                        minSize = r - l;
+                        startIndex = l;
+                    }
+                }
+                if (dict[s[l]] == 0)
+                    checker++;
                 dict[s[l]]++;
-                if (dict[s[l]] > 0)
-                    diff++;
+                l++;
             }
-            l++;
         }
-        return minimum == s.size() + 1 ? "" : s.substr(index, minimum);
+        if (startIndex == -1)
+            return "";
+        return s.substr(startIndex, minSize);
     }
 };
