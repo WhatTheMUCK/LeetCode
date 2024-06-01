b926176da463d70c6f9839ada291d16c9a112efc
GitHub Action <action@github.com>
2024-06-01 15:31:09 +0000

    Automatically rename directories

--- 0645-set-mismatch/set-mismatch.cpp
+++ 0645-set-mismatch/set-mismatch.cpp
@@ -3,16 +3,14 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         unordered_map<int, int> mp;
         int repetition;
+        int sum = 0;
         for (int elem : nums){
             if (mp.contains(elem))
                 repetition = elem;
             mp[elem]++;
+            sum += elem;
         }
-
-        for (int i = 1; i <= nums.size(); i++){
-            if (!mp.contains(i))
-                return {repetition, i};
-        }
-        return {};
+        int n = nums.size();
+        return {repetition, n * (n + 1) / 2 - (sum - repetition)};
     }
 };
