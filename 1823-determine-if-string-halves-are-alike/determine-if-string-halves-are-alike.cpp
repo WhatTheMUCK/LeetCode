beab3f9c302025823624c82c88b8bdb78e195856
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-01 18:01:41 +0300

    Time: 3 ms (55.17%) | Memory: 7.9 MB (50.21%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        string vowels = "aeiouAEIOU";
+        string vowels = "aeiou";
         int vowels1 = 0, vowels2 = 0;
         int i = 0;
         for (i; i < s.size() / 2; i++){
-            if (find(vowels.begin(), vowels.end(), s[i]) != vowels.end())
+            if (find(vowels.begin(), vowels.end(), tolower(s[i])) != vowels.end())
                 vowels1++;
         }
         for (i; i < s.size(); i++){
-            if (find(vowels.begin(), vowels.end(), s[i]) != vowels.end())
+            if (find(vowels.begin(), vowels.end(), tolower(s[i])) != vowels.end())
                 vowels2++;
         }
         return vowels1 == vowels2;
