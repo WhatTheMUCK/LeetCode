ab26ba21b0dac22f0ca66d4a60fec3279cc3fbec
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-03 09:51:10 +0300

    Time: 15 ms (87.56%) | Memory: 11.9 MB (83.42%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -4,11 +4,9 @@ public:
         int fst = 0, snd = 0;
         while (fst < s.size() && snd < t.size()){
             if (s[fst] == t[snd]){
-                fst++;
                 snd++;
-            } else {
-                fst++;
             }
+            fst++;
         }
         return (int)t.size() - snd;
     }
