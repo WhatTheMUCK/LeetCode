8cff8e6c83165b6ca563b7e18dfd376b2516baae
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-10 01:58:34 -0500

    Time: 0 ms (100.00%) | Memory: 10.2 MB (44.04%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> sorted = heights;
-        sort(sorted.begin(), sorted.end());
+        vector<int> temp(100);
+        for (int elem : heights)
+            temp[elem - 1]++;
+        vector<int> sorted;
+        for (int i = 0; i < 100; i++){
+            generate_n(back_inserter(sorted), temp[i], [i](){ return i + 1; });
+        }
         return accumulate(sorted.begin(), sorted.end(), 0, [&heights, i = 0](int acc, int x) mutable {
             return acc + (x != heights[i++]);
         });
