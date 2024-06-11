38fb09b5388dc3e052cc4b19d24ead11cee846ff
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-11 01:50:27 -0500

    Time: 2 ms (61.59%) | Memory: 9.9 MB (61.02%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         sort(arr1.begin(), arr1.end());
-        vector<int> answer, visited(arr1.size(), 0);
-        for (int i = 0; i < arr2.size(); i++){
-            vector<int>::iterator l = lower_bound(arr1.begin(), arr1.end(), arr2[i]);
-            vector<int>::iterator r = upper_bound(arr1.begin(), arr1.end(), arr2[i]);
-            generate_n(back_inserter(answer), r - l, [i, &arr2](){ return arr2[i]; });
-            for (int i = l - arr1.begin(); i < r - arr1.begin(); i++){
-                visited[i] = 1;
-            }
+        vector<int> answer;
+        unordered_map<int, int> dict;
+        for (int elem : arr1){
+            dict[elem]++;
         }
-
+        for (int i = 0; i < arr2.size(); i++){
+            generate_n(back_inserter(answer), dict[arr2[i]], [i, &arr2](){ return arr2[i]; });
+            dict.erase(arr2[i]);
+        }
         for (int i = 0; i < arr1.size(); i++){
-            if (!visited[i])
-                answer.push_back(arr1[i]);
+            if (dict.contains(arr1[i])){
+                generate_n(back_inserter(answer), dict[arr1[i]], [i, &arr1](){ return arr1[i]; });
+                dict.erase(arr1[i]);
+            }
         }
         return answer;
     }
