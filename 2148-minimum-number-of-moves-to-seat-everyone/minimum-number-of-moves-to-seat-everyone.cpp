965c3f5e4e93bd20cd971e7efaf26977dae221af
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-13 03:57:12 -0500

    Time: 7 ms (60.18%) | Memory: 21 MB (17.35%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -3,7 +3,7 @@ public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
         sort(seats.begin(), seats.end());
         sort(students.begin(), students.end());
-        return accumulate(seats.begin(), seats.end(), 0, [i = 0, &students, &seats](int acc, int x) mutable {
+        return accumulate(seats.begin(), seats.end(), 0, [i = 0, &students](int acc, int x) mutable {
             return acc + abs(students[i++] - x); 
         });
     }
