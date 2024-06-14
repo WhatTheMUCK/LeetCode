9ad82874d367ad7d721f504f436fbbfe43a55555
GitHub Action <action@github.com>
2024-06-14 20:21:24 +0000

    Automatically rename directories

--- 0049-group-anagrams/group-anagrams.cpp
+++ 0049-group-anagrams/group-anagrams.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<int, unordered_map<char, int>> mp;
+        unordered_map<string, vector<string>> mp;
         int n = strs.size();
+        string key;
         for (int i = 0; i < n; i++){
-            for (char ch : strs[i]){
-                mp[i][ch]++;
-            }
+            key = strs[i];
+            sort(key.begin(), key.end());
+            mp[key].push_back(strs[i]);
         }
-        vector<int> visited(n, 0);
         vector<vector<string>> answer;
-        for (int i = 0; i < n; i++){
-            if (visited[i] == 1)
-                continue;
-            answer.push_back({strs[i]});
-            for (int j = i + 1; j < n; j++){
-                if (mp[i] == mp[j]){
-                    answer.back().push_back(strs[j]);
-                    visited[j] = 1;
-                }
-            }
-            visited[i] = 1;
+        for (auto elem : mp){
+            answer.push_back(elem.second);
         }
         return answer;
     }
