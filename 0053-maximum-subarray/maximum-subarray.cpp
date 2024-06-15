68137be29f3a32f1a9a95110f1172ac64abc7f4e
GitHub Action <action@github.com>
2024-06-15 09:17:37 +0000

    Automatically rename directories

--- 0053-maximum-subarray/maximum-subarray.cpp
+++ 0053-maximum-subarray/maximum-subarray.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int> pref(n + 1, 0);
+        int sum = 0;
         int minpref = 0;
-        int answer = -1e5;
-        for (int i = 1; i <= n; i++){
-            pref[i] = nums[i - 1] + pref[i - 1];
-            answer = max({answer, pref[i], pref[i] - minpref});
-            minpref = min(minpref, pref[i]);
+        int answer = INT_MIN;
+        for (int i = 0; i < n; i++){
+            sum += nums[i ];
+            answer = max({answer, sum, sum - minpref});
+            minpref = min(minpref, sum);
         }
         return answer;
     }
