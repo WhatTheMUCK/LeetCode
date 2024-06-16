ef8542d41cf5226578b97fee8df85f7b5f0b429f
GitHub Action <action@github.com>
2024-06-16 10:25:35 +0000

    Automatically rename directories

--- 0074-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,16 +3,16 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int l = 0, r = n * m, median;
+        int l = 0, r = n * m - 1, median;
         int i, j;
-        while (l < r){
+        while (l <= r){
             median = (l + r) / 2;
             i = median / m;
             j = median % m;
             if (target == matrix[i][j])
                 return true;
             if (target < matrix[i][j]){
-                r = median;
+                r = median - 1;
             } else {
                 l = median + 1;
             }
