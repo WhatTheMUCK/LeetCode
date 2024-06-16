18dd55952a344848378c8b3ada4cc22746bd38c9
GitHub Action <action@github.com>
2024-06-16 19:43:04 +0000

    Automatically rename directories

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,27 +7,16 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-int search(TreeNode* cur, TreeNode* p, TreeNode* q, TreeNode* &answer){
-    int result = 0;
-    if (!cur)
-        return 0;
-    if (cur == p)
-        result = 1;
-    if (cur == q)
-        result = 2;
-    result = result ^ search(cur->left, p, q, answer) ^ search(cur->right, p, q, answer);
-    if (result == 3){
-        answer = cur;
-        result = 0;
-    }
-    return result;
-}
 
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* answer;
-        search(root, p, q, answer);
-        return answer;
+        if (root == p || root == q)
+            return root;
+        if (root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if (root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        return root;
     }
 };
