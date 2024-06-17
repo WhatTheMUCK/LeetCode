5faf5a907830fd594a693a3f86423e174a91f57e
GitHub Action <action@github.com>
2024-06-17 08:51:33 +0000

    Automatically rename directories

--- 0424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -4,8 +4,7 @@ public:
         unordered_map<char, int> mp;
         int l = 0, r = 0, maxf = 0, answer = 0;
         for (r; r < s.size(); r++){
-            mp[s[r]]++;
-            maxf = max(maxf, mp[s[r]]);
+            maxf = max(maxf, ++mp[s[r]]);
 
             if (r - l + 1 - maxf > k){
                 mp[s[l++]]--;
