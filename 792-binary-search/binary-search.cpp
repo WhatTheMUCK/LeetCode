4bd61e5e2f5793614c5b4f28f9567b5bae8ac4d2
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-17 02:53:13 -0500

    Time: 25 ms (68.18%) | Memory: 30 MB (22.96%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ind = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        if (ind == nums.size() || nums[ind] != target)
+            return -1;
+        return ind;
+    }
+};
