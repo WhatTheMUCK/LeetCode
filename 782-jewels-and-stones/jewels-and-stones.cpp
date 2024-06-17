12090a6740a7fb9e96bc1d104abd1259583c1a62
GitHub Action <action@github.com>
2024-06-17 07:29:03 +0000

    Automatically rename directories

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int numJewelsInStones(string jewels, string stones) {
-        unordered_set<char> jewelrys(jewels.begin(), jewels.end());
-        return accumulate(stones.begin(), stones.end(), 0, [&jewelrys](int acc, int x){ return acc + jewelrys.contains(x); });
-    }
-};
