29b26705c7370cd35181d1f3cf721f84c1e1f06f
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-20 05:07:06 -0500

    Time: 134 ms (25.65%) | Memory: 61.2 MB (95.31%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(), position.end());
+        int l = 1, r = 1e9, median;
+        while (l <= r){
+            median = (l + r) / 2;
+            if (check(position, m, median)){
+                l = median + 1;
+            } else {
+                r = median - 1;
+            }
+        }
+        return l - 1;
+    }
+private:
+    bool check(vector<int>& position, int m, int force){
+        int step;
+        for (int i = 0; i < position.size(); i++){
+            m--;
+            step = 0;
+            while(i + step < position.size() && position[i + step] - position[i] < force){
+                step++;
+            }
+            i += step - 1;
+        }
+        return m <= 0; 
+    }
+};
