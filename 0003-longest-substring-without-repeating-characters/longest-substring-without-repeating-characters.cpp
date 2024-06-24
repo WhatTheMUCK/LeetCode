acb5953a767314a924dfe58d33a7b1ee734377bc
GitHub Action <action@github.com>
2024-06-24 07:17:33 +0000

    Automatically rename directories

--- 0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int> dict(128, 0);
-        int l = 0, r = 0;
-        int answer = 0;
-        while (r < s.size()){
+        int l = 0, answer = 0;
+        for (int r = 0; r < s.size(); r++){
             dict[s[r]]++;
-            while (dict[s[r]] > 1){
-                dict[s[l]]--;
-                l++;
-            }
+            while (dict[s[r]] > 1)
+                dict[s[l++]]--;
             answer = max(answer, r - l + 1);
-            r++;
         }
         return answer;
     }
