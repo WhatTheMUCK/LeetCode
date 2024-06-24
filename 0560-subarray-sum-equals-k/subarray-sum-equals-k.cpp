492e1bf1fb0e490af04552f95394958098486250
GitHub Action <action@github.com>
2024-06-24 07:59:02 +0000

    Automatically rename directories

--- 0560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,22 +4,16 @@ public:
         vector<int> prefix(nums.size(), 0);
         unordered_map<int, int> mp;
         prefix[0] = nums[0];
-        mp[prefix[0]] = 0;
         for (int i = 1; i < nums.size(); i++){
             prefix[i] = prefix[i - 1] + nums[i];
-            if (mp.find(prefix[i]) == mp.end()){
-                mp[prefix[i]] = 0;
-            }
         }
 
         int answer = 0;
+        mp[0] = 1;
         for (int i = 0; i < nums.size(); i++){
-            if (prefix[i] == k)
-                answer++;
-
-            if (mp.find(prefix[i] - k) != mp.end()){
+            if (mp.contains(prefix[i] - k))
                 answer += mp[prefix[i] - k];
-            }
+            
             mp[prefix[i]]++;
         }
 
