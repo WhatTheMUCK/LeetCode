35f180dea4785339bb5f23d93909e9eb8831cd7e
GitHub Action <action@github.com>
2024-06-28 19:18:05 +0000

    Automatically rename directories

--- 0001-two-sum/two-sum.cpp
+++ 0001-two-sum/two-sum.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> search;
-        search[nums[0]] = 0;
-        for (int i = 1; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++){
             if (search.contains(target - nums[i]))
                 return {search[target - nums[i]], i};
             search[nums[i]] = i;
