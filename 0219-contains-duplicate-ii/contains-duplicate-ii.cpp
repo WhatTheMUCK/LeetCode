4f1eb4f9e6b6f0c278ee485ed84c1cc7b8998021
GitHub Action <action@github.com>
2024-06-28 19:39:26 +0000

    Automatically rename directories

--- 0219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> window;
-        int elems = 0, l = 0;
+        unordered_set<int> window;
         for (int i = 0; i < nums.size(); i++){
-            if (elems == k + 1){
-                window[nums[l++]]--;
-                elems--;
-            }
-            if (window[nums[i]] == 1)
+            if (i > k)
+                window.erase(nums[i - (k + 1)]);
+            if (window.contains(nums[i]))
                 return true;
-            window[nums[i]]++;
-            elems++;
+            window.insert(nums[i]);
         }
         return false;
     }
