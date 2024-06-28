128f008996c378c52a6be1c3aa4ed8b458d0734b
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-06-28 01:15:12 -0500

    Time: 243 ms (88.79%) | Memory: 116.3 MB (95.17%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -6,10 +6,10 @@ public:
             cityUsage[elem[0]]++;
             cityUsage[elem[1]]++;
         }
-        sort(cityUsage.begin(), cityUsage.end(), greater<int>());
+        sort(cityUsage.begin(), cityUsage.end());
         long long answer = 0;
         for (int i = 0; i < n; i++){
-            answer += 1ll * (n - i) * cityUsage[i];
+            answer += 1ll * (i + 1) * cityUsage[i];
         }
         return answer;
     }
