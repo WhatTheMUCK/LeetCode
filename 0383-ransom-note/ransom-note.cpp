73081b18c94bc4b35746d426cb295cc997310fc2
GitHub Action <action@github.com>
2024-06-28 18:26:41 +0000

    Automatically rename directories

--- 0383-ransom-note/ransom-note.cpp
+++ 0383-ransom-note/ransom-note.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> dict;
-        for (char elem : magazine){
-            if (dict.find(elem) != dict.end()){
-                dict[elem]++;
-            } else {
-                dict[elem] = 1;
-            }
-        }
-        for (char elem : ransomNote){
-            if (dict.find(elem) ==  dict.end()){
+        vector<int> letter('z' - 'a' + 1, 0);
+        for (char sign : magazine)
+            letter[sign - 'a']++;
+        for (char sign : ransomNote){
+            if (letter[sign - 'a'] == 0)
                 return false;
-            }
-            if (dict[elem] == 0){
-                return false;
-            }
-            dict[elem]--;
+            letter[sign - 'a']--;
         }
         return true;
     }
