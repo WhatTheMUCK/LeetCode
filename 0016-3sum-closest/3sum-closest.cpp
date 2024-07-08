0826340ebc19bd9c223253e2a7094b0db50a71b6
GitHub Action <action@github.com>
2024-07-08 18:35:40 +0000

    Automatically rename directories

--- 0016-3sum-closest/3sum-closest.cpp
+++ 0016-3sum-closest/3sum-closest.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        unordered_set<int> uno, duo;
-        uno.insert(nums[0]);
-        uno.insert(nums[1]);
-        duo.insert(nums[0] + nums[1]);
-        int closest = INT_MAX;
+        int absDifference = INT_MAX;
         int answer;
-        for (int i = 2; i < nums.size(); i++){
-            for (int elem : duo){
-                int temp = elem + nums[i];
-                if (abs(target - temp) < closest){
-                    closest = abs(target - temp);
-                    answer = temp;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; i++){
+            int l = i + 1, r = nums.size() - 1;
+            while (l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum < target)
+                    l++;
+                else
+                    r--;
+                if (absDifference > abs(target - sum)){
+                    absDifference = abs(target - sum);
+                    answer = sum;
                 }
             }
-            for (int elem : uno){
-                duo.insert(elem + nums[i]);
-            }
-            uno.insert(nums[i]);
         }
         return answer;
     }
