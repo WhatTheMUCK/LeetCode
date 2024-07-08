c0e4b00925f7f9f26eb60c942e5db3a1d8b660ac
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-07-08 16:01:01 -0500

    Time: 0 ms (100.00%) | Memory: 13.2 MB (61.04%) - LeetSync

--- 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.cpp
+++ 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
-        return accumulate(endTime.begin(), endTime.end(), 0, [i = 0, &startTime, queryTime](int acc, int x) mutable {
+        return accumulate(endTime.begin(), endTime.end(), 0, [i = 0, &startTime, &queryTime](int acc, int x) mutable {
             return acc + (startTime[i++] <= queryTime && queryTime <= x);
         });
     }
