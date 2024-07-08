1aec454aaec6cd397b77564ebcdbbf1b3605aa39
GitHub Action <action@github.com>
2024-07-08 21:08:44 +0000

    Automatically rename directories

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        for (int i = 0; i <= (int)haystack.size() - (int)needle.size(); i++){
-            if (haystack.substr(i, needle.size()) == needle){
-                return i;
-            }
-        }
-        return -1;
+        return haystack.find(needle);
     }
 };
