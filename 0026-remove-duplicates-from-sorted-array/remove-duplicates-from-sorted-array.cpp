3a5417e3f69fe5778502e576e9d7c13379046bfa
GitHub Action <action@github.com>
2024-07-08 21:02:52 +0000

    Automatically rename directories

--- 0026-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 1;
-        int uniq = 0;
-        while (i < nums.size()){
-            if (nums[i] == nums[uniq]){
-                i++;
-            } else {
-                uniq++;
-                nums[uniq] = nums[i]; 
-            }
-        }
-        return uniq + 1;
+       return unique(nums.begin(), nums.end()) - nums.begin();
     }
 };
