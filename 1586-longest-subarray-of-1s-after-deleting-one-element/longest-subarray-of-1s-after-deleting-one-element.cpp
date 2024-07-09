4a006f8da0a32e8490a014a34e0c67c35391ea96
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-07-09 13:30:59 -0500

    Time: 39 ms (72.70%) | Memory: 58.6 MB (76.13%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -3,27 +3,16 @@ public:
     int longestSubarray(vector<int>& nums) {
         int prev = 0, cur = 0, answer = 0;
         int i = 0;
-        bool existZero = false;
-        while (i < nums.size() && (cur == 0 || nums[i] == 1)){
-            if (nums[i] == 0){
-                existZero = true;
-            } else {
-                cur++;
-            }
-            i++;
-        }
         for (i; i < nums.size(); i++){
             if (nums[i] == 0){
-                existZero = true;
-                answer = max(answer, prev + cur);
                 prev = cur;
                 cur = 0;
             } else {
                 cur++;
             }
+            answer = max(answer, prev + cur);
         }
-        answer = max(answer, prev + cur);
-        if (!existZero)
+        if (cur == nums.size())
             answer--;
         return answer;
     }
