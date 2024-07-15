be7d65c4d52a29eef30b55c8d59996a4b0f1f726
GitHub Action <action@github.com>
2024-07-15 19:43:14 +0000

    Automatically rename directories

--- 0228-summary-ranges/summary-ranges.cpp
+++ 0228-summary-ranges/summary-ranges.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> answer;
-        if (nums.empty())
-            return answer;
         int l = 0, r = 1;
-        while (r <= nums.size()){
-            if (r == nums.size() || nums[r - 1] + 1 != nums[r]){
-                if (r - 1 == l){
-                    answer.push_back(to_string(nums[l]));
-                } else {
-                    answer.push_back(to_string(nums[l]) + "->" + to_string(nums[r - 1]));
-                }
-                l = r;
+        while (l < nums.size()){
+            while (r < nums.size() && nums[r] == nums[r - 1] + 1){
+                r++;
             }
+            if (l != r - 1)
+                answer.push_back(to_string(nums[l]) + "->" + to_string(nums[r - 1]));
+            else 
+                answer.push_back(to_string(nums[l]));
+            l = r;
             r++;
         }
         return answer;
