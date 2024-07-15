5081b65e2f1901165347d00463659807fb2f274f
GitHub Action <action@github.com>
2024-07-15 19:52:28 +0000

    Automatically rename directories

--- 0056-merge-intervals/merge-intervals.cpp
+++ 0056-merge-intervals/merge-intervals.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> answer;
         sort(intervals.begin(), intervals.end());
-        int l = intervals[0][0], r = intervals[0][1];
+        vector<vector<int>> answer;
+        int l1 = intervals[0][0], r1 = intervals[0][1];
         for (int i = 1; i < intervals.size(); i++){
-            if (intervals[i][0] > r){
-                answer.push_back({l, r});
-                l = intervals[i][0];
-                r = intervals[i][1];
+            int l2 = intervals[i][0], r2 = intervals[i][1];
+            if (l2 > r1){
+                answer.push_back({l1, r1});
+                l1 = l2;
+                r1 = r2;
             } else {
-                l = min(l, intervals[i][0]);
-                r = max(r, intervals[i][1]);
+                l1 = min(l1, l2);
+                r1 = max(r1, r2);
             }
         }
-        answer.push_back({l, r});
+        answer.push_back({l1, r1});
         return answer;
     }
 };
