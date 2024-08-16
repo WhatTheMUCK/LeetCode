4af0e74c031c91e6f1e6d58835460f1e4e356f5b
GitHub Action <action@github.com>
2024-08-16 07:16:40 +0000

    Automatically rename directories

--- 0624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -5,8 +5,7 @@ public:
         int minVal = arrays[0][0], maxVal = arrays[0].back();
         for (int i = 1; i < arrays.size(); i++){
             int tempMin = arrays[i][0], tempMax = arrays[i].back();
-            answer = max(answer, abs(maxVal - tempMin));
-            answer = max(answer, abs(minVal - tempMax));
+            answer = max({answer, abs(maxVal - tempMin), abs(minVal - tempMax)});
             minVal = min(minVal, tempMin);
             maxVal = max(maxVal, tempMax);
         }
