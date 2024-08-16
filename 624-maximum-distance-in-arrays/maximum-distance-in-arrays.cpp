31fab07f4cef76a21abfff47fee036df652fc21b
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-08-16 02:14:59 -0500

    Time: 223 ms (77.83%) | Memory: 107.8 MB (69.81%) - LeetSync

---
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int answer = 0;
+        int minVal = arrays[0][0], maxVal = arrays[0].back();
+        for (int i = 1; i < arrays.size(); i++){
+            int tempMin = arrays[i][0], tempMax = arrays[i].back();
+            answer = max(answer, abs(maxVal - tempMin));
+            answer = max(answer, abs(minVal - tempMax));
+            minVal = min(minVal, tempMin);
+            maxVal = max(maxVal, tempMax);
+        }
+        return answer;
+    }
+};
