cf69a08442c80d61b79d9f2a42ed3d71e9236f93
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-09-30 13:06:00 -0500

    Time: 33 ms (20.40%) | Memory: 25.9 MB (92.52%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -1,7 +1,6 @@
 class CustomStack {
 public:
-    deque<int> st;
-    stack<int> temp;
+    vector<int> st;
     int maxSize;
     CustomStack(int maxSize) {
         this->maxSize = maxSize;
@@ -9,25 +8,20 @@ public:
     
     void push(int x) {
         if (st.size() < maxSize)
-            st.push_front(x);
+            st.push_back(x);
     }
     
     int pop() {
         if (st.empty())
             return -1;
-        int top = st.front();
-        st.pop_front();
+        int top = st.back();
+        st.pop_back();
         return top;
     }
     
     void increment(int k, int val) {
-        for (int i = 0; i < k && !st.empty(); i++){
-            temp.push(st.back() + val);
-            st.pop_back();
-        }
-        while (!temp.empty()){
-            st.push_back(temp.top());
-            temp.pop();
+        for (int i = 0; i < k && i < st.size(); i++){
+            st[i] += val;
         }
     }
 };
