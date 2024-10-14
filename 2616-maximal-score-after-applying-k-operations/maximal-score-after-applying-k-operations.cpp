76dc12d8546b44c070915cae832a33a1a37c33a8
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-10-14 12:40:07 -0500

    Time: 188 ms (75.56%) | Memory: 76 MB (79.55%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -4,10 +4,13 @@ public:
         long long answer = 0;
         priority_queue<int> pq_nums(nums.begin(), nums.end());
         while (k--){
-            double curMax = pq_nums.top();
+            int curMax = pq_nums.top();
             pq_nums.pop();
             answer += curMax;
-            pq_nums.push(ceil(curMax / 3));
+            if (curMax % 3)
+                pq_nums.push(curMax / 3 + 1);
+            else
+                pq_nums.push(curMax / 3);
         }
         return answer;
     }
