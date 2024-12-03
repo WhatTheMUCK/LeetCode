89fb4888dc1ca12f6b5db2b8476447f307994a01
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-12-03 10:47:09 +0300

    Time: 7 ms (99.10%) | Memory: 84.7 MB (27.25%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        string answer = "";
         int n = s.size(), m = spaces.size();
+        vector<char> answer(n + m);
         int spaceInd = 0;
         for (int i = 0; i < n; i++){
             if (spaceInd < m && i == spaces[spaceInd]){
-                answer.push_back(' ');
+                answer[i + spaceInd] = ' ';
                 spaceInd++;
             }
-            answer.push_back(s[i]);
+            answer[i + spaceInd] = s[i];
         }
-        return answer;
+        return string(answer.begin(), answer.end());
     }
 };
