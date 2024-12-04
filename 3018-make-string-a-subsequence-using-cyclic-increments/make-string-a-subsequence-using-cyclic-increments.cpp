60e05be9f3f400cb67258dddd9dcbef781bec0e6
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-12-04 09:47:06 +0300

    Time: 4 ms (51.93%) | Memory: 17.3 MB (7.53%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -6,24 +6,16 @@ public:
         return 'a' + (letter - 'a' + 1) % alphabetSize;
     }
 
-    bool temp(string& str1, string& str2, int str1Ind, int str2Ind){
-        int n1 = str1.size(), n2 = str2.size();
-        if (str2Ind == n2){
-            return true;
-        }
-        if (str1Ind == n1){
-            return false;
-        }
-
-        bool option1, option2;
-        if (str1[str1Ind] == str2[str2Ind] || cyclicChar(str1[str1Ind]) == str2[str2Ind]){
-            return temp(str1, str2, str1Ind + 1, str2Ind + 1);
-        } else {
-            return temp(str1, str2, str1Ind + 1, str2Ind);
-        }
-    }
-
     bool canMakeSubsequence(string str1, string str2) {
-        return temp(str1, str2, 0, 0);
+        int n1 = str1.size(), n2 = str2.size();
+        int str1Ind = 0, str2Ind = 0;
+        while (str1Ind < n1 && str2Ind < n2){
+            if (str1[str1Ind] == str2[str2Ind] || cyclicChar(str1[str1Ind]) == str2[str2Ind]){
+                str1Ind++, str2Ind++;
+            } else {
+                str1Ind++;
+            }
+        }
+        return str2Ind == n2;
     }
 };
