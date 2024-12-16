1fd1f703950f6e5e12e932a43c34d5e587ce8e53
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2024-12-16 19:08:01 +0300

    Time: 3 ms (30.95%) | Memory: 27.8 MB (25.16%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,17 +1,7 @@
-class Compare{
-    public:
-        bool operator()(pair<int, int> left, pair<int, int> right){
-            if (left.first == right.first){
-                return left.second > right.second;
-            } 
-            return left.first > right.first;
-        }
-};
-
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> pq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
         int n = nums.size();
         for (int i = 0; i < n; i++){
             pq.push({nums[i], i});
