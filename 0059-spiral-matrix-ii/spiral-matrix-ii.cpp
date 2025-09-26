ebba6e0c904f3c283a9638df6c6525b584b3f88b
GitHub Action <action@github.com>
2025-09-26 19:42:14 +0000

    Automatically rename directories

--- 0059-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,23 +1,31 @@
+std::pair<int, int> & operator+=(std::pair<int, int> & lhs, const std::pair<int, int> & rhs) {
+    lhs.first += rhs.first;
+    lhs.second += rhs.second;
+    return lhs;
+}
+
 class Solution {
 public:
-    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
     vector<vector<int>> generateMatrix(int n) {
-        int helper = 1, counter = n * n, steps, i = 0, j = -1;
-        vector<vector<int>> answer(n, vector<int>(n));
-        while (counter){
-            for (int k = 0; k < 4; k++){
-                steps = n - (helper / 2);
-                while (counter && steps){
-                    i += directions[k][0];
-                    j += directions[k][1];
-                    steps--;
-                    counter--;
-                    answer[i][j] = n * n - counter;
+        std::vector<std::pair<int, int>> offsets = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        std::vector<std::vector<int>> answer(n, std::vector<int>(n));
+        int curValue = 1;
+        int helper = 2 * n;
+        std::pair<int, int> curPos = {0, -1};
+        while (curValue <= n * n) {
+            for (int i = 0; i < 4; ++i) {
+                for (int j = 0; j < (helper / 2); ++j) {
+                    if (curValue > n * n) {
+                        return answer;
+                    }
+                    curPos += offsets[i];
+                    auto & [row, col] = curPos;
+                    answer[row][col] = curValue++;
                 }
-                helper++;
+                --helper;
             }
         }
+
         return answer;
     }
 };
