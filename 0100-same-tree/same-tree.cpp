c83700963f8b79f50a95cd93e452d4de2e555a6f
GitHub Action <action@github.com>
2025-09-26 21:44:33 +0000

    Automatically rename directories

--- 0100-same-tree/same-tree.cpp
+++ 0100-same-tree/same-tree.cpp
@@ -12,12 +12,14 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!(p || q))
+        if (!p && !q) {
             return true;
-        if ((p || q) && !(p && q))
+        }
+
+        if (!p || !q) {
             return false;
-        if (p->val != q->val)
-            return false;
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
