a863194fb43e5ebd654d8d8b4a0404c245d49dde
GitHub Action <action@github.com>
2025-09-26 19:17:04 +0000

    Automatically rename directories

--- 0056-merge-intervals/merge-intervals.cpp
+++ 0056-merge-intervals/merge-intervals.cpp
@@ -6,7 +6,7 @@ public:
         answer.push_back(intervals[0]);
         for (size_t i = 1; i < intervals.size(); ++i) {
             if (intervals[i][0] <= answer.back()[1]) {
-                answer.back()[0] = std::min(answer.back()[0], intervals[i][0]);
+                // answer.back()[0] = std::min(answer.back()[0], intervals[i][0]);
                 answer.back()[1] = std::max(answer.back()[1], intervals[i][1]);
                 continue;
             }
