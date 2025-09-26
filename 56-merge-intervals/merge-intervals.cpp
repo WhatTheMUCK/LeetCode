53ffec8f49359c802305600e162dcf93c728130e
WhatTheMUCK <91006341+WhatTheMUCK@users.noreply.github.com>
2025-09-26 22:16:58 +0300

    Time: 3 ms (89.45%) | Memory: 23.8 MB (57.75%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        std::vector<std::vector<int>> answer;
+        std::sort(intervals.begin(), intervals.end());
+        answer.push_back(intervals[0]);
+        for (size_t i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] <= answer.back()[1]) {
+                // answer.back()[0] = std::min(answer.back()[0], intervals[i][0]);
+                answer.back()[1] = std::max(answer.back()[1], intervals[i][1]);
+                continue;
+            }
+            answer.push_back(intervals[i]);
+        }
+
+        return answer;
+    }
+};
