b658e87cbd71f62f03660bfd2c4b975eeb7b6d20
GitHub Action <action@github.com>
2025-09-26 22:19:23 +0000

    Automatically rename directories

--- 0121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy = prices[0], answer = 0;
-        for (int i = 1; i < prices.size(); i++){
-            if (prices[i] < buy)
-                buy = prices[i];
-            answer = max(answer, prices[i] - buy);
+        int answer = 0;
+        int minValue = prices[0];
+        for (size_t i = 1; i < prices.size(); ++i) {
+            answer = std::max(answer, prices[i] - minValue);
+            minValue = std::min(minValue, prices[i]);
         }
         return answer;
     }
