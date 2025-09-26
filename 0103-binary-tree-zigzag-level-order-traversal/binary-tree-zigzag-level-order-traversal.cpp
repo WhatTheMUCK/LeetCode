7574cdda51b3d23a88f11805d090bdbe7573196e
GitHub Action <action@github.com>
2025-09-26 22:05:06 +0000

    Automatically rename directories

--- 0103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,24 +12,31 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> answer;
-        queue<pair<TreeNode*, int>> bfs;
-        if (root)
-            bfs.push({root, 1});
-        while (!bfs.empty()){
-            auto [cur, level] = bfs.front();
-            bfs.pop();
-            if (!cur)
+        std::vector<std::vector<int>> answer;
+        std::queue<std::pair<TreeNode *, int>> nodes;
+        nodes.push({root, 0});
+        while (!nodes.empty()) {
+            auto [node, lvl] = nodes.front();
+            nodes.pop();
+            if (node == nullptr) {
                 continue;
-            if (level > answer.size())
-                answer.push_back({});
-            answer[level - 1].push_back(cur->val);
-            bfs.push({cur->left, level + 1});
-            bfs.push({cur->right, level + 1}); 
+            }
+
+            if (lvl == answer.size()) {
+                answer.push_back({node->val});
+            } else {
+                answer[lvl].push_back(node->val);
+            }
+
+            ++lvl;
+            nodes.push({node->left, lvl});
+            nodes.push({node->right, lvl});
         }
-        for (int i = 1; i < answer.size(); i += 2){
-            reverse(answer[i].begin(), answer[i].end());
+
+        for (size_t i = 1; i < answer.size(); i += 2) {
+            std::reverse(answer[i].begin(), answer[i].end());
         }
+
         return answer;
     }
 };
