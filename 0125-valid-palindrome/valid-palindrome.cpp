97437b81e20ed68bb2ff9059d5e41925cc49f14b
GitHub Action <action@github.com>
2025-09-26 22:24:49 +0000

    Automatically rename directories

--- 0125-valid-palindrome/valid-palindrome.cpp
+++ 0125-valid-palindrome/valid-palindrome.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int l = 0, r = s.size() - 1;
-        while (l < r){
-            while (l < r && !isalnum(s[l]))
-                l++;
-            while (l < r && !isalnum(s[r]))
-                r--;
-            if (tolower(s[l]) != tolower(s[r]))
+        int l = 0, r = static_cast<int>(s.size());
+        while (l < r) {
+            while (l < r && !std::isalnum(s[l])) {
+                ++l;
+            }
+
+            while(l < r && !std::isalnum(s[r])) {
+                --r;
+            }
+
+            if (std::tolower(s[l]) != std::tolower(s[r])) {
                 return false;
-            l++;
-            r--;
+            }
+
+            ++l;
+            --r;
         }
         return true;
     }
