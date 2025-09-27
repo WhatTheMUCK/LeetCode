3e4c473638259cbdcc9105bc1d3ad44a1f73b3b6
GitHub Action <action@github.com>
2025-09-27 18:26:07 +0000

    Automatically rename directories

--- 0228-summary-ranges/summary-ranges.cpp
+++ 0228-summary-ranges/summary-ranges.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        vector<string> answer;
-        int l = 0, r = 1;
-        while (l < nums.size()){
-            while (r < nums.size() && nums[r] == nums[r - 1] + 1){
-                r++;
-            }
-            if (l != r - 1)
-                answer.push_back(to_string(nums[l]) + "->" + to_string(nums[r - 1]));
-            else 
-                answer.push_back(to_string(nums[l]));
-            l = r;
-            r++;
+        int n = static_cast<int>(nums.size());
+        std::vector<std::string> answer;
+        if (n == 0) {
+            return answer;
         }
+
+        int lhs = nums[0], curInd = 1;
+        bool covering = false;
+        for (; curInd < n; ++curInd) {
+            if (nums[curInd] == nums[curInd - 1] + 1) {
+                covering = true;
+                continue;
+            }
+
+            if (nums[curInd - 1] == lhs) {
+                answer.push_back(std::to_string(lhs));
+            } else {
+                answer.push_back(std::to_string(lhs) + "->" + std::to_string(nums[curInd - 1]));
+            }
+            lhs = nums[curInd];
+            covering = false;
+        }
+
+        if (covering) {
+            answer.push_back(std::to_string(lhs) + "->" + std::to_string(nums.back()));
+        } else {
+            answer.push_back(std::to_string(nums.back()));
+        }
+
         return answer;
     }
 };
