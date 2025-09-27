044a65407f53485f1fdf4108f081d0c8cc97ca35
GitHub Action <action@github.com>
2025-09-27 19:12:53 +0000

    Automatically rename directories

--- 0236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,27 +7,42 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-int search(TreeNode* cur, TreeNode* p, TreeNode* q, TreeNode* &answer){
-    int result = 0;
-    if (!cur)
-        return 0;
-    if (cur == p)
-        result = 1;
-    if (cur == q)
-        result = 2;
-    result = result ^ search(cur->left, p, q, answer) ^ search(cur->right, p, q, answer);
-    if (result == 3){
-        answer = cur;
-        result = 0;
-    }
-    return result;
-}
+#include <unordered_map>
 
 class Solution {
 public:
+    std::unordered_map<TreeNode *, std::string> mp;
+    std::string preprocess(TreeNode * root) {
+        if (!root) {
+            return "";
+        }
+
+        if (root->left) {
+            mp[root->left] = mp[root] + '/';
+            preprocess(root->left);
+        }
+
+        if (root->right) {
+            mp[root->right] = mp[root] + '\\';
+            preprocess(root->right);
+        }
+
+        return mp[root]; 
+    }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* answer;
-        search(root, p, q, answer);
-        return answer;
+        preprocess(root);
+        std::string pPath = mp[p], qPath = mp[q];
+        size_t i = 0;
+        for (; i < pPath.size() && i < qPath.size() && pPath[i] == qPath[i]; ++i);
+        std::string path = qPath.substr(0, i);
+        for (char elem : path) {
+            if (elem == '/') {
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+        }
+        return root;
     }
 };
