9b248e10cb64ec8f6d1a356a41a1231f4eee8918
GitHub Action <action@github.com>
2025-09-27 15:57:08 +0000

    Automatically rename directories

--- 0658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int l = 0, r = arr.size() - 1, m;
-        while (l <= r){
-            m = (l + r) / 2;
-            //cout << "l: " << l << " r: " << r << " m: " << m << " x: " << x << " arr[m]: " << arr[m] << "\n";
-            if (x >= arr[m]){
-                l = m + 1;
-            } else{
-                r = m - 1;
-            } 
+        int n = static_cast<int>(arr.size());
+        if (k >= n) {
+            return arr;
         }
-        deque<int> closest;
-        //int start = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
-        //int l = start - 1, r = start;
-        r = l, l = l - 1;
-        while (k > 0){
-            if (l < 0){
-                closest.push_back(arr[r]);
-                r++;
-            } else if (r >= arr.size()){
-                closest.push_front(arr[l]);
-                l--;
-            } else {
-                if (abs(arr[l] - x) <= abs(arr[r] - x)){
-                    closest.push_front(arr[l]);
-                    l--;
-                } else {
-                    closest.push_back(arr[r]);
-                    r++;
-                }
+
+        std::deque<int> answer;
+        std::vector<int>::iterator it = std::lower_bound(arr.begin(), arr.end(), x);
+        if (it == arr.end()) {
+            return std::vector<int>(arr.begin() + (n - k), arr.end());
+        }
+
+        int r = it - arr.begin(), l = r - 1;
+        while (l >= 0 && r < n && answer.size() < k) {
+            int diffL = std::abs(x - arr[l]), diffR = std::abs(x - arr[r]);
+            if (diffL <= diffR) {
+                answer.push_front(arr[l]);
+                --l;
+                continue;
             }
-            k--;
+
+            answer.push_back(arr[r]);
+            ++r;
         }
 
-        vector<int> answer(closest.begin(), closest.end());
+        while (l >= 0 && answer.size() < k) {
+            answer.push_front(arr[l]);
+            --l;
+        }
 
-        return answer;
+        while (r < n && answer.size() < k) {
+            answer.push_back(arr[r]);
+            ++r;
+        }
+
+        return std::vector<int>(answer.begin(), answer.end());
     }
 };
