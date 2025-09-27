cee74ff61861ce4df10e69ac9e2153b91dc622da
GitHub Action <action@github.com>
2025-09-27 16:45:50 +0000

    Automatically rename directories

--- 0680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-
-    bool isPalindrome(string s, int l, int r, int freeErase){
-        while (l <= r){
-            if (s[l] == s[r]){
-                l++;
-                r--;
-            } else {
-                if (freeErase == 0)
-                    return false;
-                return (isPalindrome(s, l, r - 1, freeErase - 1) || isPalindrome(s, l + 1, r, freeErase - 1));
+    bool validPalindrome(const std::string & s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return false;
             }
+            ++l, --r;
         }
         return true;
     }
+
     bool validPalindrome(string s) {
-        int l = 0, r = s.size() - 1, freeErase = 1;
-        return isPalindrome(s, l, r, freeErase);
+        int l = 0, r = static_cast<int>(s.size()) - 1;
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return validPalindrome(s, l + 1, r) || validPalindrome(s, l, r - 1);
+            }
+            ++l, --r;
+        }
+        return true;
     }
 };
