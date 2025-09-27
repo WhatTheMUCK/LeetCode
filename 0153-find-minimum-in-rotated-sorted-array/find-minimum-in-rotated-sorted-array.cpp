165ec26d8a7d611adfe9355c98bef728fe9f0088
GitHub Action <action@github.com>
2025-09-27 16:26:04 +0000

    Automatically rename directories

--- 0153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int l = 0, r = nums.size() - 1, m;
-        while (l < r){
-            m = (l + r) / 2;
-            if (nums[l] < nums[r]){
-                return nums[l];
-            } else {
-                if (nums[m] > nums[r]){
-                    l = m + 1;
-                } else {
-                    r = m;
-                }
+        int l = 0, r = static_cast<int>(nums.size()) - 1;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (nums[l] <= nums[m] && nums[m] <= nums[r] || nums[l] > nums[m] && nums[m] <= nums[r]) {
+                r = m;
+            } else if (nums[l] <= nums[m] && nums[m] > nums[r] || nums[l] > nums[m] && nums[m] > nums[r]) {
+                l = m + 1;
             }
         }
         return nums[l];
+
+        // 0 1 2 3 4 5 6 7
+        // 3 4 5 6 7 1 2
+        //         .   .  
     }
 };
