ba1cf5076742782fb8a392e5bd340364edf00d29
GitHub Action <action@github.com>
2025-09-28 20:42:56 +0000

    Automatically rename directories

--- 0792-binary-search/binary-search.cpp
+++ 0792-binary-search/binary-search.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1, m;
-        while (l <= r){
-            m = (l + r) / 2;
-            if (nums[m] == target)
+        int n = static_cast<int>(nums.size());
+        int l = 0, r = n;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (target == nums[m]) {
                 return m;
-            else if (nums[m] > target)
+            }
+
+            if (target < nums[m]) {
                 r = m - 1;
-            else 
+            } else {
                 l = m + 1;
+            }
         }
-        return -1;
+        if (l == n || nums[l] != target) {
+            return -1;
+        }
+
+        return l;
     }
 };
