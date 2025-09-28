314b0d71d14e217efb756d5b7ff7276843e9f020
GitHub Action <action@github.com>
2025-09-28 19:56:47 +0000

    Automatically rename directories

--- 268-missing-number/missing-number.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n = static_cast<int>(nums.size());
-        return (n * (n + 1)) / 2 - accumulate(nums.begin(), nums.end(), 0);
-    }
-};
