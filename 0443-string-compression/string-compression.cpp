1394fe8d0b51e6cca1c6382dbeaa040594782d8b
GitHub Action <action@github.com>
2025-09-29 12:36:33 +0000

    Automatically rename directories

--- 0443-string-compression/string-compression.cpp
+++ 0443-string-compression/string-compression.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        vector<char> answer;
-        int l = 0, r = 1;
-        while (r <= chars.size()){
-            if (r == chars.size() || chars[l] != chars[r]){
-                answer.push_back(chars[l]);
-                if (r - l > 1){
-                    string number = to_string(r - l);
-                    generate_n(back_inserter(answer), number.size(), [i = 0, &number]() mutable { return number[i++]; });
-                }
-                l = r;
+        int prevPos = 0;
+        size_t l = 0, r = 0;
+        for (; r <= chars.size(); ++r) {
+            if (r < chars.size() && chars[r] == chars[prevPos]) {
+                continue;
             }
-            r++;
+            
+            int amount = r - prevPos;
+            prevPos = r;
+            chars[l] = chars[r - 1];
+            if (amount != 1) {
+                string tmp = to_string(amount);
+                for (char digit : tmp) {
+                    ++l;
+                    chars[l] = digit;
+                }
+            }
+            ++l;
         }
-        chars = answer;
-        return chars.size();
+
+        return l;
     }
 };
