415599c6e857b11b9f77131e8eb50ee255288043
GitHub Action <action@github.com>
2025-09-29 11:13:18 +0000

    Automatically rename directories

--- 0879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
+++ 0879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
-        vector<int> closestLeft(seats.size(), -1e6), closestRight(seats.size(), 1e6);
-        for (int i = 1; i < seats.size(); i++){
-            if (seats[i - 1] == 1){
-                closestLeft[i] = i - 1;
+        int n = static_cast<int>(seats.size());
+        vector<int> closestLeft(n + 1, -1e6), closestRight(n + 1, 1e6);
+        for (int i = 0; i < n; ++i) {
+            if (seats[i] == 1) {
+                closestLeft[i + 1] = i;
             } else {
-                closestLeft[i] = closestLeft[i - 1]; 
+                closestLeft[i + 1] = closestLeft[i];
             }
         }
 
-        // for (int i = 0; i < closestLeft.size(); i++){
-        //     cout << closestLeft[i] << " ";
-        // }
-        // cout << "\n";
-
-        for (int i = seats.size() - 2; i >= 0; i--){
-            if (seats[i + 1] == 1){
-                closestRight[i] = i + 1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (seats[i] == 1) {
+                closestRight[i] = i;
             } else {
                 closestRight[i] = closestRight[i + 1];
             }
         }
 
-        // for (int i = 0; i < closestRight.size(); i++){
-        //     cout << closestRight[i] << " ";
-        // }
-        // cout << "\n";
+        int maxDistance = 0;
 
-        int maxDistance = 0, distance;
-        for (int i = 0; i < seats.size(); i++){
-            distance = min(closestRight[i] - i, i - closestLeft[i]);
-            if (distance > maxDistance && seats[i] == 0){
-                maxDistance = distance;
-            }
+        for (int i = 0; i < n; ++i) {
+            int tempDistance = min(i - closestLeft[i + 1], closestRight[i] - i);
+            maxDistance = max(maxDistance, tempDistance);
         }
+
         return maxDistance;
     }
 };
