d15637ebf662bab72d6f0a35d65f925192ef2ed3
GitHub Action <action@github.com>
2025-09-29 13:03:45 +0000

    Automatically rename directories

--- 0975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 0975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -12,12 +12,15 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        if (!root)
+        if (!root) {
             return 0;
-        if (root->val < low)
-            return rangeSumBST(root->right, low, high);
-        if (root->val > high)
-            return rangeSumBST(root->left, low, high);    
-        return root->val + rangeSumBST(root->right, low, high) + rangeSumBST(root->left, low, high);
+        }
+
+        int curSum = 0;
+        if (low <= root->val && root->val <= high) {
+            curSum = root->val;
+        }
+
+        return curSum + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
     }
 };
