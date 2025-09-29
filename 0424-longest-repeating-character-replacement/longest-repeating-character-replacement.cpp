62657ed7b796fbeec706c236483bc0e324ef4461
GitHub Action <action@github.com>
2025-09-29 12:04:40 +0000

    Automatically rename directories

--- 0424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char, int> mp;
-        int l = 0, r = 0, maxf = 0, answer = 0;
-        for (r; r < s.size(); r++){
-            maxf = max(maxf, ++mp[s[r]]);
-
-            if (r - l + 1 - maxf > k){
-                mp[s[l++]]--;
-            } else {
-                answer = max(answer, r - l + 1);
+        int n = static_cast<int>(s.size());
+        
+        int answer = 0;
+        string dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (char letter : dictionary) {
+            int tempK = k;
+            int l = 0, r = 0;
+            while (r < n) {
+                answer = max(answer, r - l);
+                if (s[r] != letter) {
+                    if (tempK > 0) {
+                        --tempK;
+                        ++r;
+                        continue;
+                    } else {
+                        tempK += s[l] != letter;
+                        ++l;
+                        continue; 
+                    }
+                }
+                ++r;
             }
+            answer = max(answer, r - l);
         }
+
         return answer;
     }
 };
