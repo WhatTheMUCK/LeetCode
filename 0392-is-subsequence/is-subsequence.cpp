d01a218eb1d15ff121bd72b96d6c1281bac9f5e1
GitHub Action <action@github.com>
2025-09-29 11:49:34 +0000

    Automatically rename directories

--- 0392-is-subsequence/is-subsequence.cpp
+++ 0392-is-subsequence/is-subsequence.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int l1 = 0, l2 = 0;
-        while (l1 < s.size() && l2 < t.size()){
-            if (s[l1] == t[l2]){
-                l1++;
-                l2++;
-            } else {
-                l2++;
+        size_t firstIndex = 0, secondIndex = 0;
+        while (firstIndex < s.size() && secondIndex < t.size()) {
+            if (s[firstIndex] == t[secondIndex]) {
+                ++firstIndex;
             }
+
+            ++secondIndex;
         }
-        if (l1 == s.size()){
-            return true;
-        }
-        return false;
+
+        return firstIndex == s.size();
     }
 };
