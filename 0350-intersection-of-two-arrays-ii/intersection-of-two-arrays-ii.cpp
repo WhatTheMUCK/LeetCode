a7524a37cfab37bb7e88550c5bb485df8771a16f
GitHub Action <action@github.com>
2025-09-29 10:50:46 +0000

    Automatically rename directories

--- 0350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int>& v1, vector<int>& v2) {
-        unordered_map<int, int> nums;
-        vector<int> answer;
-        for (int elem : v1){
-            nums[elem]++;
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() < nums2.size()) {
+            return intersect(nums2, nums1);
         }
-        for (int elem : v2){
-            if (nums[elem] > 0){
-                answer.push_back(elem);
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> answer;
+        size_t firstInd = 0, secondInd = 0;
+        while (firstInd < nums1.size() && secondInd < nums2.size()) {
+            if (nums1[firstInd] == nums2[secondInd]) {
+                answer.push_back(nums1[firstInd]);
+                ++firstInd, ++ secondInd;
+                continue;
             }
-            nums[elem]--;
+
+            if (nums1[firstInd] < nums2[secondInd]) {
+                ++firstInd;
+                continue;
+            }
+
+            if (nums1[firstInd] > nums2[secondInd]) {
+                ++secondInd;
+                continue;
+            }
         }
         return answer;
-    }  
+    }
 };
