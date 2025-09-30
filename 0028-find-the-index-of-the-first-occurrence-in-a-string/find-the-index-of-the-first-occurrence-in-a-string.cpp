1959df9464f2d80760282e482f8f2191786f4825
GitHub Action <action@github.com>
2025-09-30 12:47:48 +0000

    Automatically rename directories

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int> zFunction(string input) {
-        int n = static_cast<int>(input.size());
+    vector<int> zFunction(const string & s) {
+        int n = static_cast<int>(s.size());
         vector<int> z(n, 0);
         int l = 0, r = 0;
         for (int i = 1; i < n; ++i) {
@@ -9,7 +9,7 @@ public:
                 z[i] = min(r - i + 1, z[i - l]);
             }
 
-            while (i + z[i] < n && input[z[i]] == input[i + z[i]]) {
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
                 ++z[i];
             }
 
@@ -18,17 +18,20 @@ public:
                 r = i + z[i] - 1;
             }
         }
+
         return z;
     }
-
+ 
     int strStr(string haystack, string needle) {
         vector<int> z = zFunction(needle + '#' + haystack);
-
-        for (size_t curPos = needle.size() + 1; curPos < z.size(); ++curPos) {
-            if (z[curPos] == needle.size()) {
-                return curPos - (needle.size() + 1);
+        int n = static_cast<int>(needle.size());
+        int m = static_cast<int>(haystack.size());
+        for (int i = n + 1; i < n + m + 1; ++i) {
+            if (z[i] == n) {
+                return i - (n + 1);
             }
         }
+
         return -1;
     }
 };
