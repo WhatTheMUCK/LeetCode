51e7a2c3d050145b9918d779cd65e0c122a0e7fb
GitHub Action <action@github.com>
2025-09-30 09:11:03 +0000

    Automatically rename directories

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+    vector<int> zFunction(string input) {
+        int n = static_cast<int>(input.size());
+        vector<int> z(n, 0);
+        int l = 0, r = 0;
+        for (int i = 1; i < n; ++i) {
+            if (i <= r) {
+                z[i] = min(r - i + 1, z[i - l]);
+            }
+
+            while (i + z[i] < n && input[z[i]] == input[i + z[i]]) {
+                ++z[i];
+            }
+
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
+    }
+
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        vector<int> z = zFunction(needle + '#' + haystack);
+
+        for (size_t curPos = needle.size() + 1; curPos < z.size(); ++curPos) {
+            if (z[curPos] == needle.size()) {
+                return curPos - (needle.size() + 1);
+            }
+        }
+        return -1;
     }
 };
